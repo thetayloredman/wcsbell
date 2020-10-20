fad678e23b3d271939dd95da941bd20a585997a7
thetayloredman <nutdriver716@gmail.com>
2020-10-20 16:34:46 -0700

    foo

--- index.js
+++ index.js
@@ -3,13 +3,22 @@
 // For Willits Charter School
 
 // Modules
-const fs = require('fs');
+const fs = require('fs').promises;
+const express = require('express');
+const enmap = require('enmap');
 
-// Init
-console.log('wcsbell');
-console.log('Loading files...')
+// Main
+(async () => {
+    console.clear();
+    console.log('wcsbell');
+    console.log('By Logan Devine');
+    console.log('For Willits Charter School');
+    console.log('');
+    console.log('Please wait as WCSBell initializes...');
+    console.log('This may take a while...');
+    // Initialize DBs
+    const config = new enmap({ name: 'config' });
 
-// Init config
-let config = new Map();
-
-// Initialize
+    // Post init
+    console.log('Initialized!');
+})();
