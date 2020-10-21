49a09459f44c8cefd5bc26edaa5c9ae2347595eb
thetayloredman <nutdriver716@gmail.com>
2020-10-20 19:44:39 -0700

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,1 +1,10 @@
-document.getElementById('root').innerHTML = `<h1>WCSBell Setup</h1>`;
+document.getElementById("root").innerHTML = `<h1>WCSBell Setup</h1>
+<h2>Let's get you set up!</h2>
+<div id="status">
+    <h2>Current Status</h2>
+    <div>
+        <ol>
+    
+        </ol>
+    </div>
+</div>`;
