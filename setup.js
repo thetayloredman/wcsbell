255c58600ba89946584104cc13c94323416fcd99
thetayloredman <nutdriver716@gmail.com>
2020-10-20 20:37:09 -0700

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,12 +1,15 @@
-const disabledcb = '<input type="checkbox" onclick="return false;" />';
-const enabledcb = '<input type="checkbox" onclick="return false;" checked />'
 document.getElementById("root").innerHTML = `<h1>WCSBell Setup</h1>
 <h2>Let's get you set up!</h2>
+<div id="shameless-plug">
+    <h2>Powered by <img src="https://gblobscdn.gitbook.com/assets%2F-LFnuGgkksxzZuqQoImA%2F-MBdbxAuOrLq6T7TJwqt%2F-MBdgEKrowQXDmYg7IoQ%2Fenmap-logo.svg?alt=media&token=fe6b6a83-029f-4914-87da-a993ce90b8ee" alt="Enmap" /> by <i>Evie.Codes</i></h2>
+    <h3>Check out Enmap here: <a href="https://evie.dev/enmap/bbhc">Enmap</a></h3>
+</div>
 <div id="status">
     <h2>Current Status</h2>
     <div>
         <ol>
-            <li><input type="checkbox" onclick="return false;" /> <i></i></li>
+            <li><input type="checkbox" onclick="return false;" id="cb-1" /> <i>Set a Password</i></li>
+            <li><input type="checkbox" onclick="return false;" id="cb-2" /> <i>Add your Bell Sound</i></li>
         </ol>
     </div>
 </div>`;
