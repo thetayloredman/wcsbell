d7801e8cf3ab586386205c7273b1e22ab25f241c
thetayloredman <nutdriver716@gmail.com>
2020-10-20 19:48:07 -0700

    Update setup.js

--- setup.js
+++ setup.js
@@ -1,10 +1,12 @@
+const disabledcb = '<input type="checkbox" onclick="return false;" />';
+const enabledcb = '<input type="checkbox" onclick="return false;" checked />'
 document.getElementById("root").innerHTML = `<h1>WCSBell Setup</h1>
 <h2>Let's get you set up!</h2>
 <div id="status">
     <h2>Current Status</h2>
     <div>
         <ol>
-            <li><input type="checkbox" onclick="return false;" checked="" />
+            <li><input type="checkbox" onclick="return false;" /> <i></i></li>
         </ol>
     </div>
 </div>`;
